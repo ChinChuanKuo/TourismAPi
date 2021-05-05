752c8e8219fc59384f056da8590e2762b4c45a4f
ChinChuanKuo <chinchuankuo@alberthuang1.flytech.com.tw>
2021-05-05 13:18:28 +0800

    module better

--- Models/InsertModels.cs
+++ Models/InsertModels.cs
@@ -46,6 +46,7 @@ namespace tourismAPi.Models
                     return new statusModels() { showWarn = true, status = $"This Person is from the future？" };
                 }
             }
+            int i = 0;
             database database = new database();
             foreach (var item in dataitems)
             {
@@ -63,11 +64,12 @@ namespace tourismAPi.Models
                 dbparamlist.Add(new dbparam("@traffic", traffic ? "搭車" : "自行前往"));
                 dbparamlist.Add(new dbparam("@location", traffic ? location ? "內湖" : "林口" : ""));
                 dbparamlist.Add(new dbparam("@ago", dateTime.Year - DateTime.Parse(DateTime.ParseExact(item["birthday"].ToString().Trim(), "yyyyMMdd", CultureInfo.InvariantCulture).ToString("yyyy/MM/dd")).Year));
-                dbparamlist.Add(new dbparam("@money", new MoneyClass().checkOffice(item["userid"].ToString().Trim()) ? traffic ? 0 : -200 : new MoneyClass().checkMoney(categoryId, traffic, bool.Parse(item["showPlace"].ToString().TrimEnd()), item["birthday"].ToString().Trim())));
+                dbparamlist.Add(new dbparam("@money", new MoneyClass().checkOffice(item["userid"].ToString().Trim()) ? traffic ? 0 : -200 : new MoneyClass().checkMoney(categoryId, i, traffic, bool.Parse(item["showPlace"].ToString().TrimEnd()), item["birthday"].ToString().Trim())));
                 if (database.checkActiveSql("mssql", "sysstring", "exec web.inserttourismitem @userid,@username,@idcard,@birthday,@category,@traffic,@location,@ago,@money;", dbparamlist) != "istrue")
                 {
                     return new statusModels() { showWarn = true, status = "Please contact the engineer" };
                 }
+                i++;
             }
             return new statusModels() { showWarn = false, status = "Congratulations, Information Has Been Updated" };
         }
--- Models/MoneyModels.cs
+++ Models/MoneyModels.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using tourismAPi.App_Code;
 
 namespace tourismAPi.Models
@@ -12,27 +14,20 @@ namespace tourismAPi.Models
         {
             int total = 0;
             bool showTraffic = bool.Parse(traffic);
-            foreach (var item in JsonSerializer.Deserialize<List<Dictionary<string, object>>>(items))
+            foreach (var item in JsonSerializer.Deserialize<List<Dictionary<string, object>>>(items).Select((value, i) => new { i, value }))
             {
-                bool showPlace = bool.Parse(item["showPlace"].ToString().Trim());
-                switch (item["userid"].ToString().Trim())
+                bool showPlace = bool.Parse(item.value["showPlace"].ToString().Trim());
+                int money = checkMoney(categoryId, item.i, showTraffic, showPlace, item.value["birthday"].ToString().Trim());
+                switch (item.value["userid"].ToString().Trim())
                 {
                     case "":
-                        total += checkMoney(categoryId, showTraffic, showPlace, item["birthday"].ToString().Trim());
+                        total += money < 0 ? 0 : money;
                         break;
                     default:
-                        switch (checkOffice(item["userid"].ToString().Trim()))
+                        switch (checkOffice(item.value["userid"].ToString().Trim()))
                         {
-                            case true:
-                                switch (showTraffic)
-                                {
-                                    case false:
-                                        total -= 200;
-                                        break;
-                                }
-                                break;
-                            default:
-                                total += checkMoney(categoryId, showTraffic, showPlace, item["birthday"].ToString().Trim());
+                            case false:
+                                total += money < 0 ? 0 : money;
                                 break;
                         }
                         break;
@@ -49,12 +44,10 @@ namespace tourismAPi.Models
             return new database().checkSelectSql("mssql", "sysstring", "select username from web.siteber where userid = @userid;", dbparamlist).Rows.Count > 0;
         }
 
-        public int checkMoney(string categoryId, bool traffic, bool place, string day)
+        public int checkMoney(string categoryId, int count, bool traffic, bool place, string day)
         {
-            switch (string.IsNullOrWhiteSpace(day))
-            {
-                case true:
-                    return 0;
+            if (!Regex.IsMatch(day, @"^[0-9]+$") || day.Length != 8) {
+                return 0;
             }
             DateTime nowDate = DateTime.Now, birthday = DateTime.Parse(DateTime.ParseExact(day.ToString().Trim(), "yyyyMMdd", CultureInfo.InvariantCulture).ToString("yyyy/MM/dd"));
             int year = nowDate.Year - birthday.Year;
@@ -63,12 +56,28 @@ namespace tourismAPi.Models
                 switch (categoryId)
                 {
                     case "0":
-                        return 1500;
+                        switch (traffic)
+                        {
+                            case true:
+                                return 1500;
+                        }
+                        return 1240;
+                }
+                switch (count)
+                {
+                    case 1:
+                        switch (traffic)
+                        {
+                            case true:
+                                return 200;
+                            default:
+                                return -300;
+                        }
                 }
                 switch (traffic)
                 {
                     case true:
-                        return 900;
+                        return 860;
                     default:
                         return 600;
                 }
@@ -78,12 +87,28 @@ namespace tourismAPi.Models
                 switch (categoryId)
                 {
                     case "0":
-                        return 1100;
+                        switch (traffic)
+                        {
+                            case true:
+                                return 1100;
+                        }
+                        return 840;
+                }
+                switch (count)
+                {
+                    case 1:
+                        switch (traffic)
+                        {
+                            case true:
+                                return 200;
+                            default:
+                                return -300;
+                        }
                 }
                 switch (traffic)
                 {
                     case true:
-                        return 900;
+                        return 860;
                     default:
                         return 600;
                 }
@@ -93,18 +118,74 @@ namespace tourismAPi.Models
                 switch (categoryId)
                 {
                     case "0":
-                        return 650;
-                    default:
+                        switch (traffic)
+                        {
+                            case true:
+                                return 650;
+                        }
+                        return 390;
+                }
+                switch (count)
+                {
+                    case 1:
+                        switch (traffic)
+                        {
+                            case true:
+                                return 100;
+                            default:
+                                return -400;
+                        }
+                }
+                switch (traffic)
+                {
+                    case true:
                         return 730;
+                    default:
+                        return 470;
                 }
             }
-            switch (place)
+            switch (categoryId)
             {
-                case true:
-                    return 300;
-                default:
+                case "0":
+                    switch (traffic)
+                    {
+                        case true:
+                            switch (place)
+                            {
+                                case true:
+                                    return 300;
+                            }
+                            return 40;
+                    }
                     return 40;
             }
+            switch (count)
+            {
+                case 1:
+                    switch (traffic)
+                    {
+                        case true:
+                            switch (place)
+                            {
+                                case true:
+                                    return -300;
+                            }
+                            return -400;
+                        default:
+                            return -500;
+                    }
+            }
+            switch (traffic)
+            {
+                case true:
+                    switch (place)
+                    {
+                        case true:
+                            return 300;
+                    }
+                    return 40;
+            }
+            return 40;
         }
     }
 }
